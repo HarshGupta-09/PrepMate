83423fedccad7a9652a352ee2aaef30579fc21c3
Harsh Gupta <harshgupta82003@gmail.com>
2026-07-09 15:59:23 +0530

    tested apiResponse.js and apiError.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 const app = express();
+import apiResponse from "./utils/ApiResponse.js"
+import apiError from "./utils/ApiError.js"
 
 
 app.use(express.json())
@@ -10,10 +12,16 @@ app.use(express.urlencoded({ extended : true }))
 
 
 app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "PrepMate API is running."
-    });
+   const user = {
+    name : "Harsh",
+    role : "Student"
+   };
+    console.log(new apiResponse(200,user,"tested successfullyy"))
+
+  const error = new apiError(404,"user not found")
+  console.log(error.message)
+
+
 });
 
 export default app;
