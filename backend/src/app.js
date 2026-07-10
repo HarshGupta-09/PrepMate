6f05b98461aaf9fdc63b3567230641c957ae74a8
Harsh Gupta <harshgupta82003@gmail.com>
2026-07-10 11:47:21 +0530

    feat : added notFound route  middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,35 @@
 import express from "express";
+
+import ApiResponse from "./utils/ApiResponse.js";
+import notFoundMiddleware from "./middlewares/notFound.middleware.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+
 const app = express();
-import apiResponse from "./utils/ApiResponse.js"
-import apiError from "./utils/ApiError.js"
 
+// Built-in Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended : true }))
-
-
-
-
-
+// Test Route
 app.get("/", (req, res) => {
-   const user = {
-    name : "Harsh",
-    role : "Student"
-   };
-    
+    const user = {
+        name: "Harsh",
+        role: "Student",
+    };
 
-  
-
-
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            user, 
+            "User fetched successfully"
+        )
+    );
 });
+
+// Handles Undefined Routes
+app.use(notFoundMiddleware);
+
+// Global Error Handler 
+app.use(errorMiddleware);
 
 export default app;
