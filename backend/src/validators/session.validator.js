cea97dfc83251df95d3de20d7e5efcf6689bebe2
Harsh Gupta <harshgupta82003@gmail.com>
2026-09-11 15:31:27 +0530

    added session validation schema

---
+++ backend/src/validators/session.validator.js
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const sessionSchema = z.object({
+    role: z.string().trim().min(1),
+
+    experience: z.number().min(0),
+
+    topics: z.array(
+        z.string().trim().min(1)
+    ).min(1),
+
+    difficulty: z.enum([
+        "easy",
+        "medium",
+        "hard"
+    ]),
+});
